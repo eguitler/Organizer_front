7efd5c51051b0c268849afacf199ccd8388a54c3
Ezequiel Guitler <eguitler@gmail.com>
2022-03-10 00:17:28 -0300

    added PRIOTITIES to constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,20 @@
 export const IS_DEVELOPMENT = process.env.REACT_APP_ENV === 'dev';
 export const API_URL = process.env.REACT_APP_API_URL;
+
+export const PRIORITIES = [
+  {
+    value: 0,
+    label: 'Low',
+    icon: '💧',
+  },
+  {
+    value: 1,
+    label: 'Medium',
+    icon: '➖',
+  },
+  {
+    value: 2,
+    label: 'High',
+    icon: '🔥',
+  },
+];
